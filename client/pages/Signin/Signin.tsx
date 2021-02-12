df5b5905574896a3bc7d82ddc0843d3eeb049e38
Marat <m.mukhutdinov@agora.ru>
2021-02-12 21:41:46 +0800

    LP-18: Сделал страницу входа, временно убрал навигацию

--- client/pages/Signin/Signin.tsx
+++ client/pages/Signin/Signin.tsx
@@ -12,3 +12,22 @@ export const Signin: React.FC<PageComponentProps> = React.memo(({ title }) => (
         </main>
     </div>
 ));
+// import { PageComponentProps } from 'shared/types';
+// import { Link } from 'react-router-dom';
+// import { ROUTES } from 'routing';
+
+
+// export const Signin: React.FC<PageComponentProps> = ({ title }) => (
+// 	<div className="page-container">
+// 		<div className="auth-container">
+// 			<h1 className="auth-header">{title}</h1>
+// 			<form>
+// 				<input placeholder="Логин" className="form-input_primary"></input>
+// 				<input placeholder="Пароль" className="form-input_primary"></input>
+// 				<input value="Авторизоваться" type="submit" className="primary-button" />
+// 			</form>
+// 			<Link to={ROUTES.SIGNUP.path} className="help-button">Нет аккаунта?</Link>
+// 		</div>
+// 	</div>
+
+// );
