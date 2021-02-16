49b278d9796a2ed94c3eb1be6dae399d02946b06
Airat Mustaev (slynell) <31629779+AirMust@users.noreply.github.com>
2021-02-16 12:46:56 +0300

    Lp 32 (#34)
    * Добавил общую тему для material

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-
-import './App.css';
-import './shared/styles/theme.css';
-
 import {
-    BrowserRouter,
+	BrowserRouter,
 } from 'react-router-dom';
 import { Routing } from 'client/routing/Routing';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import './shared/styles/theme.css';
+import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+	type: "dark",
+    primary: {
+		light: red[50],
+		main: red[500],
+		dark: red[700]
+    },
+  },
+});
 export const App: React.FC = () => (
-	<BrowserRouter>
-		<Routing />
-	</BrowserRouter>
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<BrowserRouter>
+			<Routing />
+		</BrowserRouter>
+	</ThemeProvider>
 );
