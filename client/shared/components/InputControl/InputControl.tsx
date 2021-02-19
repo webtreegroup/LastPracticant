346f971bb77cbb80f3bbf3933bfbe9ba2b974666
webtree <web-tree@yandex.ru>
2021-02-19 15:04:43 +0400

    LP-38 привел форму в минимальный рабочий вид

--- client/shared/components/InputControl/InputControl.tsx
+++ client/shared/components/InputControl/InputControl.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { FieldError } from 'react-hook-form';
-import classnames from 'classnames';
 import { ComponentCommonProps } from 'client/shared/types';
 import './InputControl.css';
-import { OutlinedInput } from '@material-ui/core';
 
 export interface InputControlProps extends ComponentCommonProps {
     name: string
@@ -21,23 +19,19 @@ InputControlProps
 >(
     (
         {
-            label,
             name,
             error,
             errorMessage = 'ошибка',
             type = 'text',
-            className,
         },
         ref,
     ) => (
         <div>
-            <OutlinedInput
+            <input
                 name={name}
-                placeholder={label}
                 ref={ref}
                 id={name}
                 type={type}
-                className={classnames('input-control', className)}
             />
             <span>{error && errorMessage}</span>
         </div>
