550c6c199bef6cf8927e7e50494d4202e1558e3d
webtree <web-tree@yandex.ru>
2021-02-19 07:39:54 +0400

    LP-52 переделал коннект к редакс на хуки

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -5,40 +5,32 @@ import './Home.css';
 import { Loader, Paper } from 'client/shared/components';
 import { Button, Grid } from '@material-ui/core';
 import { Navigation } from 'client/core';
-import { connect, ConnectedProps } from 'react-redux';
+import {
+    useDispatch, useSelector,
+} from 'react-redux';
 import { showLoaderAction, hideLoaderAction, StoreProps } from 'client/core/store';
 
-const mapStateToProps = (store: StoreProps) => ({
-    loader: store.loader,
-});
+export const Home: React.FC<PageComponentProps> = React.memo(() => {
+    const loader = useSelector((store: StoreProps) => ({
+        isVisible: store.loader,
+    }));
 
-const mapDispatchToProps = {
-    showLoaderAction,
-    hideLoaderAction,
-};
+    const dispatch = useDispatch();
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-const HomeComponent: React.FC<PageComponentProps & PropsFromRedux> = React.memo(({
-    loader,
-    showLoaderAction: showLoader,
-    hideLoaderAction: hideLoader,
-
-}) => {
     /** TODO: просто для примера, потом убрать */
     const handleShowLoader = () => {
-        showLoader();
+        dispatch(showLoaderAction());
 
-        setTimeout(hideLoader, 2000);
+        setTimeout(() => {
+            dispatch(hideLoaderAction());
+        }, 2000);
     };
 
     return (
         <Grid className="home" container justify="center" alignItems="center">
             <Paper sizes="small">
                 <Navigation />
-                <Loader isVisible={loader} />
+                <Loader isVisible={loader.isVisible} />
                 <Button
                     variant="contained"
                     color="secondary"
@@ -51,5 +43,3 @@ const HomeComponent: React.FC<PageComponentProps & PropsFromRedux> = React.memo(
         </Grid>
     );
 });
-
-export const Home = connector(HomeComponent);
