5444038a759dd7927f297da723d29f0232b3d1e8
webtree <web-tree@yandex.ru>
2021-02-19 14:58:34 +0400

    LP-53 подключил redux-devtools-extension к проекту

--- client/core/store/store.ts
+++ client/core/store/store.ts
@@ -1,10 +1,21 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { loaderReducers } from './reducers';
 
-const rootReducer = combineReducers({ loader: loaderReducers });
+const middlewares = [thunk];
+
+const rootReducer = combineReducers(
+    { loader: loaderReducers },
+);
 
 export interface StoreProps {
     loader: boolean
 }
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(...middlewares),
+    ),
+);
