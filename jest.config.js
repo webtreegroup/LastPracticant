2e1c0f0877a84a42c22f311c2734ca5d66fc7086
Айрат <airat.must@yandex.ru>
2021-03-04 11:13:14 +0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    roots: ['<rootDir>/'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    verbose: true,
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
+};
